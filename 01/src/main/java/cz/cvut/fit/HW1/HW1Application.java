bdb5722703e3fc245709f211ee93a7dde84fbe44
Razvie-arr <vladbyastinov@mail.ru>
2022-09-26 23:32:40 +0200

    oops...

---
+++ 01/src/main/java/cz/cvut/fit/HW1/HW1Application.java
@@ -0,0 +1,11 @@
+package cz.cvut.fit.HW1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HW1Application {
+    public static void main(String[] args) {
+        SpringApplication.run(HW1Application.class, args);
+    }
+}
