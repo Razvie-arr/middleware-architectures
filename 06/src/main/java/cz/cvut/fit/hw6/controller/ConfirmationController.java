4db3c8535e75d207165fa3e0bf6b90b0bf5ae1b3
Razvie-arr <vladbyastinov@mail.ru>
2022-11-28 09:48:47 +0100

    HW6 - change confirmation post method

--- 06/src/main/java/cz/cvut/fit/hw6/controller/ConfirmationController.java
+++ 06/src/main/java/cz/cvut/fit/hw6/controller/ConfirmationController.java
@@ -4,10 +4,7 @@ import cz.cvut.fit.hw6.service.ConfirmationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/confirmation")
@@ -16,8 +13,8 @@ public class ConfirmationController {
     @Autowired
     ConfirmationService confirmationService;
 
-    @PostMapping
-    public ResponseEntity<String> confirmConfirmation(@RequestParam String id) {
+    @PostMapping("/{id}")
+    public ResponseEntity<String> confirmConfirmation(@PathVariable String id) {
         confirmationService.confirmConfirmation(id);
 
         return ResponseEntity.status(HttpStatus.OK).body("Transaction confirmed");
